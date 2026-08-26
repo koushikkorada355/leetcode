cb620d6b069e844bb5587bd3faf79635e7ba4c26
Koushik Korada <koushikkorada27@gmail.com>
2026-08-26 19:54:21 +0530

    Time: 12 ms (45.85%) | Memory: 14 MB (38.75%) - LeetSync

--- 2340-total-appeal-of-a-string/total-appeal-of-a-string.cpp
+++ 2340-total-appeal-of-a-string/total-appeal-of-a-string.cpp
@@ -3,21 +3,16 @@ using namespace std;
 class Solution {
 public:
     long long appealSum(string s) {
-        long long n = s.size();
+        int n = s.size();
+        long long ans = 0;
         vector<long long> dp(26,0);
-        vector<int> left(26,0);
-        long long total = 0;
-        for(long long l = 0;l < n;l++){
-            total = total + (l + 1) * (n - l);
-        }
         for(int i = 0;i < n;i++){
             int c = s[i] - 'a';
-            left[c] = (i + 1);
-            dp[c] += (i + 1);
-            for(int k = 0;k < 26;k++){
-                total = total - dp[k] + left[k];
+            dp[c] = (i + 1);
+            for(int l = 0;l < 26;l++){
+                ans += dp[l];
             }
         }
-        return total;
+        return ans;
     }
 };
