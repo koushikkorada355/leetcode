12bdab54940ab26d2a30941506331d0b4f42ea44
Koushik Korada <koushikkorada27@gmail.com>
2026-09-08 10:35:23 +0530

    Time: 679 ms (5.13%) | Memory: 9.2 MB (5.52%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int ans = 0;
+        for(int i = 1;i <= n;i++){
+            string s = to_string(i);
+            ans += (s.size() - 1) / 3;
+        }
+        return ans;
+    }
+};
